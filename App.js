d3cdfcf1153967f2a40a889d70c784d68d17fdc4
임석현 <sjsh1623@flow.team>
2022-08-15 18:09:25 +0900

    Status bar Immersive and Landscape

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>오케이</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="translucent" />
     </View>
   );
 }
